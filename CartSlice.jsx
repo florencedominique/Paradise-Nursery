e030f1977897be6429a3b9d729d725ef72feddc1
florencedominique <422002951@ntc.edu.ph>
2026-01-06 13:08:39 +0800

    CartSlice.jsx

---
+++ CartSlice.jsx
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      state.items.push({ ...action.payload, quantity: 1 });
+    },
+    increaseQty: (state, action) => {
+      const item = state.items.find(i => i.id === action.payload);
+      if (item) item.quantity++;
+    },
+    decreaseQty: (state, action) => {
+      const item = state.items.find(i => i.id === action.payload);
+      if (item && item.quantity > 1) item.quantity--;
+    },
+    removeItem: (state, action) => {
+      state.items = state.items.filter(i => i.id !== action.payload);
+    },
+  },
+});
+
+export const { addToCart, increaseQty, decreaseQty, removeItem } =
+  cartSlice.actions;
+
+export default cartSlice.reducer;
